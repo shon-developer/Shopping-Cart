64262e951576e3b072bfe8705180e8d6020ec4e2
shon <shonbwd@gmail.com>
2023-01-20 19:13:23 +0530

    Modified File

---
+++ src/data.js
@@ -0,0 +1,30 @@
+let shopItemData = [
+  {
+    id: "shon",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "./images/img-1.jpg",
+  },
+  {
+    id: "shine",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "./images/img-2.jpg",
+  },
+  {
+    id: "zareel",
+    name: "T Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "./images/img-3.jpg",
+  },
+  {
+    id: "coding",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "./images/img-4.jpg",
+  },
+];
--- src/main.js
+++ src/main.js
@@ -1,42 +1,12 @@
 let shop = document.getElementById("shop");
 
-let shopItemData = [
-  {
-    id: "shon",
-    name: "Casual Shirt",
-    price: 45,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "./images/img-1.jpg",
-  },
-  {
-    id: "shine",
-    name: "Office Shirt",
-    price: 100,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "./images/img-2.jpg",
-  },
-  {
-    id: "zareel",
-    name: "T Shirt",
-    price: 25,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "./images/img-3.jpg",
-  },
-  {
-    id: "coding",
-    name: "Mens Suit",
-    price: 300,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "./images/img-4.jpg",
-  },
-];
-
-let basket = [];
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let generateShop = () => {
   return (shop.innerHTML = shopItemData
     .map((x) => {
       let { id, name, price, desc, img } = x;
+      let search = basket.find((x) => x.id === id) || [];
       return `
     <div id=product-id-${id} class="item">
     <img width="220" src=${x.img} alt="" />
@@ -47,7 +17,9 @@ let generateShop = () => {
         <h2>$ ${price}</h2>
         <div class="buttons">
           <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-          <div id=${id} class="quantity">0</div>
+          <div id=${id} class="quantity">
+          ${search.item === undefined ? 0 : search.item}
+          </div>
           <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
         </div>
       </div>
@@ -57,6 +29,7 @@ let generateShop = () => {
     })
     .join(""));
 };
+
 generateShop();
 
 let increment = (id) => {
@@ -71,19 +44,25 @@ let increment = (id) => {
   } else {
     search.item += 1;
   }
+
+  basket = basket.filter((x) => x.item !== 0);
   // console.log(basket);
   update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
 };
 let decrement = (id) => {
   let selectedItem = id;
   let search = basket.find((x) => x.id === selectedItem.id);
-
-  if (search.item === 0) return;
+  if (search === undefined) return;
+  else if (search.item === 0) return;
   else {
     search.item -= 1;
   }
-  // console.log(basket);
   update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  // console.log(basket);
+
+  localStorage.setItem("data", JSON.stringify(basket));
 };
 let update = (id) => {
   let search = basket.find((x) => x.id === id);
@@ -93,5 +72,8 @@ let update = (id) => {
 };
 
 let calculation = () => {
-  console.log("calculation function is running");
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerText = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
 };
+
+calculation();
